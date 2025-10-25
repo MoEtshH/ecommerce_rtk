e2ea330534f32965ce1bfba3c6ea23e3609d250e
Mohamed Hamed <mhmd.h.hamed@gmail.com>
2025-10-25 12:49:34 +0200

    solution

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,6 +1,59 @@
+import { createSlice } from "@reduxjs/toolkit";
 
-const CartSlice = ({
- 
+const initialState = {
+  cartItems: [],
+};
+
+const CartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItemToCart: (state, action) => {
+      const existingItem = state.cartItems.find(
+        (item) => item.id === action.payload.id
+      );
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        // if action.payload === { id: 3, name: "Shoe", price: 20 }
+        //{ ...action.payload, quantity: 1 }
+        // becomes:
+        //{ id: 3, name: "Shoe", price: 20, quantity: 1 }
+        state.cartItems.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    removeItemFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter(
+        (cartItem) => action.payload != cartItem.id
+      );
+    },
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    increaseItemQuantity: (state, action) => {
+      const existingItem = state.cartItems.find(
+        (item) => item.id === action.payload
+      );
+      if (existingItem) {
+        existingItem.quantity += 1;
+      }
+    },
+    decreaseItemQuantity: (state, action) => {
+      const existingItem = state.cartItems.find(
+        (item) => item.id === action.payload
+      );
+      if (existingItem && existingItem.quantity > 1) {
+        existingItem.quantity -= 1;
+      }
+    },
+  },
 });
 
-
+export const {
+  addItemToCart,
+  removeItemFromCart,
+  clearCart,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+} = CartSlice.actions;
+export default CartSlice.reducer;
---
+++ src/Components/SuperCoin.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+const SuperCoin = () => {
+  const [superCoins, setSuperCoins] = useState(0);
+  const cartItems = useSelector((state) => state.cart.cartItems);
+
+  const totalAmount = cartItems.reduce(
+    (acc, item) => acc + item.quantity * item.price,
+    0
+  );
+
+  useEffect(() => {
+    if (totalAmount >= 100 && totalAmount < 200) {
+      setSuperCoins(10);
+    } else if (totalAmount >= 200 && totalAmount < 300) {
+      setSuperCoins(20);
+    } else if (totalAmount >= 300) {
+      setSuperCoins(30);
+    } else {
+      setSuperCoins(0);
+    }
+    console.log("totalAmount", totalAmount);
+    console.log("hena", superCoins);
+  }, [totalAmount]);
+
+  return (
+    <>
+      <div className="super-coins" style={{ textAlign: "center" }}>
+        <h2 className="super-coins-title">Super Coins</h2>
+        <p className="super-coins-info">
+          You will earn {superCoins} super coins with this purchase.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default SuperCoin;
